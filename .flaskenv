993f6baea7081f79d4bde5add8d05c96245ef487
Brandon Tallman <bgtall96@gmail.com>
2025-03-31 20:42:06 -0700

    Added base and index template. Reworked init to separate configuration into its own file/class. Created a home and index route to get started.

---
+++ .flaskenv
@@ -0,0 +1,2 @@
+FLASK_APP=liftr.py
+FLASK_DEBUG=1
--- app/__init__.py
+++ app/__init__.py
@@ -1,20 +1,16 @@
-import os
 from flask import Flask
+from config import Config
 
 
-def create_app(test_config=None):
-    app = Flask(__name__, instance_path=True)
-    app.config.from_mapping(SECRET_KEY='dev', DATABASE=os.path.join(app.instance_path, 'app.sqlite'),
-    )
 
-    if test_config is None:
-        app.config.from_pyfile('config.py', silent=True)
-    else:
-        app.config.from_mapping(test_config)
+app = Flask(__name__)
+app.config.from_object(Config)
 
-    try:
-        os.makedirs(app.instance_path)
-    except OSError:
-        pass
 
-    
+from app import routes
+
+
+
+
+
+   
