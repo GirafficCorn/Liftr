c6dc54bd15b1da7efdf3b60718cbeec913102031
Brandon Tallman <bgtall96@gmail.com>
2025-04-03 16:59:00 -0700

    added login and registration forms, templates, and routes

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,7 @@
-from flask import render_template, url_for
+from flask import render_template, url_for, flash, redirect
 from app import app
 import datetime
+from app.forms import RegistrationForm, LoginForm
 
 
 workout = {
@@ -15,12 +16,31 @@ workout = {
 
 
 @app.route('/')
-@app.route('/index')
-def index():
-    return render_template('index.html', title='Home')
+@app.route('/home')
+def home():
+    return render_template('home.html', title='Home')
 
 
 @app.route('/workout')
 def workouts():
     return render_template('workout.html', title='Workouts', workout=workout)
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Register', form=form)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'test@test.com' and form.password.data == 'test':
+            flash(f'Login Successful!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful', 'danger')
+    return render_template('login.html', title='Login', form=form)
     
